442879b68991842f9468f8b50e85a8a4beaace50
dvmbnkr <a1870619@adelaide.edu.au>
2025-10-21 20:08:12 +1030

    mergesort.c updated

--- mergesort.c
+++ mergesort.c
@@ -14,19 +14,19 @@ void merge(int leftstart, int leftend, int rightstart, int rightend){
 	m = rightstart; // "Middle" or "Start of Right side"
 	k = leftstart; // Start of the temporary array to copy
 	while(s <= leftend && m <= rightend){ // At the start
-		if(array[s] <= array[m]){ // If the element in the most left subarray is smaller or equal to the element in the most right subarray
-			temp[k++] = array[s++]; // Temporarily store the smallest compared value
+		if(A[s] <= A[m]){ // If the element in the most left subarray is smaller or equal to the element in the most right subarray
+			B[k++] = A[s++]; // Temporarily store the smallest compared value
 		}else{
-			temp[k++] = array[m++]; // If it is not the case, then store the value from the right - which would be the smallest. 
+			B[k++] = A[m++]; // If it is not the case, then store the value from the right - which would be the smallest. 
 		}
 	} // Repeat this until it reaches the end of either subarray which indicates it has compared all the elements in that subarray
 	while(s <= leftend){ // If there are remaining elements in the left subarray -- Incase its not perfect half
-		temp[k++] = array[s++]; // Store them in the temporary array
+		B[k++] = A[s++]; // Store them in the temporary array
 	}
 	while(m <= rightend){ // If there are remaining elements in the right subarray -- Incase its not perfect half
-		temp[k++] = array[m++]; // Store them in the temporary array
+		B[k++] = A[m++]; // Store them in the temporary array
 	}
-	memcpy(&array[leftstart], &temp[leftstart], (rightend - leftstart + 1) * sizeof(int)); // Copy the sorted elements back to the original array like advised on Prac Page. 
+	memcpy(&A[leftstart], &B[leftstart], (rightend - leftstart + 1) * sizeof(int)); // Copy the sorted elements back to the original array like advised on Prac Page. 
 
 }
 
@@ -34,7 +34,7 @@ void merge(int leftstart, int leftend, int rightstart, int rightend){
 void my_mergesort(int left, int right){
 	int mid;
 	if (left < right){
-		mid = l + (right - left) / 2; // Find the mid point
+		mid = left + (right - left) / 2; // Find the mid point
 		my_mergesort(left, mid); // Recursively sort the left half
 		my_mergesort(mid + 1, right); // Recursively sort the right half
 		merge(left, mid, mid+1, right); // Merge the two halves
@@ -43,7 +43,9 @@ void my_mergesort(int left, int right){
 
 /* this function will be called by the testing program. */
 void * parallel_mergesort(void *arg){
-		return NULL;
+		
+	return NULL;
+
 }
 
 /* we build the argument for the parallel_mergesort function. */
